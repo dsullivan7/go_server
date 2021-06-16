2f93b16e98e49906aea6a0b8be5811f5057de9fc
Danny Sullivan <dbsullivan23@gmail.com>
2021-06-16 17:27:16 -0400

    checkpoint for reversion back to original

--- dbinit.sh
+++ dbinit.sh
@@ -4,8 +4,8 @@ echo "DB_DROP: ${DB_DROP}"
 
 if [[ ${DB_DROP} = 'yes' ]]; then
   echo "dropping database ${DB_NAME}"
-  dropdb -h ${DB_HOST} -p ${DB_PORT} -U ${DB_USER} ${DB_NAME}
+  PGPASSWORD=${DB_PASSWORD} dropdb -h ${DB_HOST} -p ${DB_PORT} -U ${DB_USER} ${DB_NAME}
 
   echo "creating database ${DB_NAME}"
-  createdb -h ${DB_HOST} -p ${DB_PORT} -U ${DB_USER} ${DB_NAME}
+  PGPASSWORD=${DB_PASSWORD} createdb -h ${DB_HOST} -p ${DB_PORT} -U ${DB_USER} ${DB_NAME}
 fi
