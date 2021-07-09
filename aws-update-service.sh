94ecd44e439395fe8167eca27edefacafdc2efea
Danny Sullivan <dbsullivan23@gmail.com>
2021-07-09 12:50:52 -0400

    adding circleci configuration

--- aws-update-service.sh
+++ aws-update-service.sh
@@ -1,5 +1,5 @@
 aws ecs update-service \
   --cluster tchr-voice-cluster \
   --service tchr-voice-service-app \
-  --task-definition tchr-voice-app:19 \
+  --task-definition tchr-voice-app:21 \
   --force-new-deployment
