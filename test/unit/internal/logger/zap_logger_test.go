cbc0be8fad4391f86809ef80e1fa5f6de3a9ad67
Danny Sullivan <dbsullivan23@gmail.com>
2021-08-20 13:07:38 -0400

    passing test for logger

--- test/unit/internal/logger/zap_logger_test.go
+++ test/unit/internal/logger/zap_logger_test.go
@@ -3,21 +3,38 @@ package logger_test
 import (
 	"go_server/internal/logger"
 	"testing"
+	"encoding/json"
 
 	"github.com/stretchr/testify/assert"
+
 	"go.uber.org/zap"
+	"go.uber.org/zap/zapcore"
+	"go.uber.org/zap/zaptest/observer"
 )
 
 func TestZapLogger(t *testing.T) {
 	t.Parallel()
 
-	zapLogger, errZap := zap.NewProduction()
-	assert.Nil(t, errZap)
+	core, recordedLogs := observer.New(zapcore.InfoLevel)
+	zapLogger := zap.New(core)
 
 	logger := logger.NewZapLogger(zapLogger)
 
-	m := make(map[string]interface{})
-	m["some"] = "key"
+	logger.InfoWithMeta(
+		"blah",
+		map[string]interface{}{
+			"some": "key",
+		},
+	)
 
-	logger.InfoWithMeta("blah", m)
+	logs := recordedLogs.All()
+
+	assert.Equal(t, 1, len(logs))
+
+	var logJSON map[string]string
+
+	errDecode := json.Unmarshal([]byte(`{"some":"key"}`), &logJSON)
+	assert.Nil(t, errDecode)
+
+	assert.Equal(t, "key", logJSON["some"])
 }
