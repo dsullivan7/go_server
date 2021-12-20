bf4a45eb5bc65053c02bd213b0ca527e85c10f7e
Danny Sullivan <dbsullivan23@gmail.com>
2021-12-20 14:20:06 -0500

    lint fix

--- internal/server/controllers/health.go
+++ internal/server/controllers/health.go
@@ -2,11 +2,12 @@ package controllers
 
 import (
 	"net/http"
-  "github.com/go-chi/render"
+
+	"github.com/go-chi/render"
 )
 
 func (c *Controllers) GetHealth(w http.ResponseWriter, r *http.Request) {
-  response := map[string]string{"status": "healthy"}
+	response := map[string]string{"status": "healthy"}
 
-  render.JSON(w, r, response)
+	render.JSON(w, r, response)
 }
