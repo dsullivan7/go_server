8c92cf85a2d45da31002d0708e107bcbd527c1a7
Danny Sullivan <dbsullivan23@gmail.com>
2022-03-09 16:16:50 -0500

    adding updated seeders

--- internal/db/seeders/development/20220116_securities.sql
+++ internal/db/seeders/development/20220116_securities.sql
@@ -1,11 +1,47 @@
 insert into securities (security_id, symbol, name, beta)
   values
     ('2cad1f30-178c-4e71-ab83-d96d5442d85a', 'TSM', 'TSMC', 1.0),
-    ('3e63e020-5def-466c-b9ca-b9f7cfa8f6e4', 'ISRG', 'Intuitive Surgical', 1.0),
+    ('3e63e020-5def-466c-b9ca-b9f7cfa8f6e4', 'CDNS', 'Cadence', 1.0),
     ('d0574809-ad38-46dd-a75e-0d5efe587374', 'AMT', 'American Tower', 1.0),
-    ('ba8b0758-975f-4d61-b20c-c7497304b2c0', 'MSCI', 'MSCI', 1.0),
-    ('dd6b61be-1b07-4d92-b173-592c450edb6d', 'MTN', 'Vail Resorts', 1.0),
-    ('a404de86-f52b-4a20-8803-510f81d28176', 'HUM', 'Humana', 1.0),
-    ('890cb4f5-8b5a-4e03-a49b-ede188ab3a16', 'IDXX', 'Idexx Laboratories', 1.0),
+
+    ('ba8b0758-975f-4d61-b20c-c7497304b2c0', 'WDAY', 'Workday', 1.0),
+    ('dd6b61be-1b07-4d92-b173-592c450edb6d', 'NOW', 'Service Now', 1.0),
+    ('a404de86-f52b-4a20-8803-510f81d28176', 'TEAM', 'Atlassian', 1.0),
+
+    ('890cb4f5-8b5a-4e03-a49b-ede188ab3a16', 'MSCI', 'MSCI', 1.0),
     ('ada0e7f8-138d-491f-aef2-38bc9cc15a33', 'BX', 'Blackstone', 1.0),
-    ('5cf7e9a0-08d1-4628-8e29-d6df1b92568f', 'SBUX', 'Starbucks', 1.0);
+    ('5cf7e9a0-08d1-4628-8e29-d6df1b92568f', 'HOOD', 'Robinhood', 1.0),
+
+    ('eef08767-b965-46eb-af86-32df5d17182f', 'HUM', 'Humana', 1.0),
+    ('cdcfb444-a699-4eb8-9169-b3daaa3a37f9', 'TMO', 'Thermo Fisher', 1.0),
+    ('b1af0ab3-7abc-459e-8e09-6b6a79beede7', 'ARGX', 'argenx', 1.0),
+
+    ('56ce9381-ccae-4639-8909-a9adc3b2b948', 'SQ', 'Square', 1.0),
+    ('b8c1f0f8-0346-48e0-bc16-7cb94d9582cf', 'SHOP', 'Shopify', 1.0),
+    ('e25de939-2aad-47d4-a5bc-208f027471a5', 'FIS', 'Fidelity National Info Services', 1.0),
+
+    ('627dc6c7-e22b-4020-b455-4a483efc9fb1', 'EQIX', 'Equnix', 1.0),
+    ('20c5016c-2628-45d4-9012-ed70aff660bb', 'CRWD', 'Crowdstrike', 1.0),
+    ('f7e29cac-182f-4dcc-a35b-c082e4c301cc', 'PANW', 'Palo Alto', 1.0),
+
+    ('dc625819-00ac-456c-8a65-2d69cee9e64f', 'FWONK', 'Liberty Media', 1.0),
+    ('80db0cb0-5d3a-483b-bf3d-be40ea65e159', 'MTCH', 'Match', 1.0),
+    ('4b954f79-c8ec-490a-92e7-07ea102ec0e7', 'MTN', 'Vail Resorts', 1.0),
+
+    ('916e3236-dcfe-46ef-bf2a-6e38d4a39dde', 'IDXX', 'Idexx Laboratories', 1.0),
+    ('3d9e6f8f-e039-482b-a4d5-114a2cb24bbd', 'CHWY', 'Chewy', 1.0),
+
+    ('cb674dc9-78f4-41e1-bf5d-3a441a566ad2', 'RIVN', 'Rivian', 1.0),
+    ('58d19c6e-2cd2-4eee-a309-0cdbe32d53f6', 'XPEV', 'Xpeng', 1.0),
+    ('aea01bac-56f0-4bbc-a008-59b928f9464f', 'LI', 'Li Auto', 1.0),
+
+    ('6f7d3d2e-b059-44b8-a33b-8eb7ae25a1f7', 'TTWO', 'Take-Two', 1.0),
+    ('8dfcfed0-a436-4e75-9c31-4721c9a2836a', 'SONY', 'Sony', 1.0),
+
+    ('a4da1236-40d8-4796-b526-7e3291d6aa9e', 'TCEHY', 'Tencent', 1.0),
+    ('eaf9fdb8-83bb-4b5a-a012-c959ce2474ec', 'JD', 'JD', 1.0),
+    ('2fbbb3b4-38b5-4acb-b5ce-909ea5fcddb3', 'HDFC', 'HDFC', 1.0),
+
+    ('6c52c67e-0b16-452a-8016-6ecfcdda9720', 'AMD', 'AMD', 1.0),
+    ('207d4611-6233-449f-ac91-e1ce54265400', 'GM', 'GM', 1.0),
+    ('7c4ac6e2-7897-4f9c-891f-84a035cdcc19', 'ORCL', 'Oracle', 1.0);
