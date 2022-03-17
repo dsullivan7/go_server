cf39dea394d7d0abc7fb1512b8d561b3a4d04737
Danny Sullivan <dbsullivan23@gmail.com>
2022-03-17 10:32:33 -0400

    fixing function

--- internal/services/portfolio.go
+++ internal/services/portfolio.go
@@ -33,7 +33,6 @@ const roundValue = 10000
 
 // ListPortfolioHoldings retreives a set of portfolio holdings
 // according to the specified portfolio and portfolio tags.
-// nolint:funlen
 func (srvc *Service) ListPortfolioHoldings(
 	portfolio models.Portfolio,
 	portfolioTags []models.PortfolioTag,
@@ -84,9 +83,6 @@ func (srvc *Service) ListPortfolioHoldings(
 		// round the amount
 		amount = math.Round(amount*roundValue) / roundValue
 		remaining -= amount
-
-		println(securityID) //nolint
-		println(amount)     //nolint
 
 		portfolioHoldings[currentIndex] = PortfolioHolding{
 			Symbol: securityMap[securityID].Symbol,
