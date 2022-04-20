4d30807a108fa41186e612a73fda709acf4d604d
Danny Sullivan <dbsullivan23@gmail.com>
2022-04-20 17:14:44 -0400

    refactor

--- internal/services/orders.go
+++ internal/services/orders.go
@@ -5,6 +5,41 @@ import (
 	"go_server/internal/models"
 )
 
+func (srvc *Service) getAssetOrders(openOrders []models.Order, val int) []models.Order {
+	var orders []models.Order
+	// if the target val is greater than zero, create a child order to take the remaining asset value
+	if (val > 0) {
+		remainingSecurityValue := val
+		for _, openOrder := range openOrders {
+			// break the loop if no security value remains
+			if (remainingSecurityValue <= 0) {
+				break
+			}
+
+			// calculate the total amount of the order that has already been covered
+			totalOrderCovered := 0
+			for _, childOrder := range openOrder.ChildOrders {
+				totalOrderCovered += childOrder.Amount
+			}
+
+			// set the amount of the child order to be
+			// the min of the remaining security value and the amount left in the parent order
+			childOrderAmount := int(math.Min(float64(remainingSecurityValue), float64(openOrder.Amount - totalOrderCovered)))
+
+			childOrder := models.Order{
+				ParentOrderID: &openOrder.OrderID,
+				Amount: childOrderAmount,
+				Side: "buy",
+			}
+
+			orders = append(orders, childOrder)
+			remainingSecurityValue -= childOrderAmount
+		}
+	}
+
+	return orders
+}
+
 // ListOrders returns what orders need to be made to resolve the market under the given parameters.
 func (srvc *Service) GetOrders(
   openOrders []models.Order,
@@ -26,35 +61,11 @@ func (srvc *Service) GetOrders(
 		}
 	}
 
-	// if netSecurityValue is greater than zero, create a child order to take the remaining asset value
-	if (netSecurityValue > 0) {
-		remainingSecurityValue := netSecurityValue
-		for _, openOrderBuy := range openOrdersBuy {
-			// break the loop if no security value remains
-			if (remainingSecurityValue <= 0) {
-				break
-			}
+	childBuyOrders := srvc.getAssetOrders(openOrdersBuy, netSecurityValue)
 
-			// calculate the total amount of the order that has already been covered
-			totalOrderCovered := 0
-			for _, childOrder := range openOrderBuy.ChildOrders {
-				totalOrderCovered += childOrder.Amount
-			}
+	// openOrderBuy.ChildOrders = append(openOrderBuy.ChildOrders, childOrder)
 
-			// set the amount of the child order to be the min of the remaining security value and the amount left in the parent order 
-			childOrderAmount := int(math.Min(float64(remainingSecurityValue), float64(openOrderBuy.Amount - totalOrderCovered)))
-
-			childOrder := models.Order{
-				ParentOrderID: &openOrderBuy.OrderID,
-				Amount: childOrderAmount,
-				Side: "buy",
-			}
-
-			openOrderBuy.ChildOrders = append(openOrderBuy.ChildOrders, childOrder)
-			orders = append(orders, childOrder)
-			remainingSecurityValue -= childOrderAmount
-		}
-	}
+	orders = append(orders, childBuyOrders...)
 
 	return orders
 }
