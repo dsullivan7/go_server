3407b6b1e79271955d71a4358a9501a9c05641eb
Danny Sullivan <danny@stairsfinancial.com>
2022-11-11 17:41:20 -0500

    removing posts

--- internal/db/seeders/development/20221002_posts.sql
+++
@@ -1,4 +0,0 @@
-insert into posts (group_id, name, message, url)
-  values
-    ('799c9f9b-8368-4bf0-b28b-697f70ecdf36', 'Job Post 1', 'Hey, we are hiring', 'https://linkedin.com'),
-    ('799c9f9b-8368-4bf0-b28b-697f70ecdf36', 'Job Post 2', 'Hey, we are hiring again', 'https://linkedin.com');
